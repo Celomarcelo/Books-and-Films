9fdfc567835c2d610696732732363f2e58a54d48
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-08-20 20:31:49 +0100

    created review in models
    added some field in reviews
    added rest_framework and reviews app in settings.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    author_director = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
