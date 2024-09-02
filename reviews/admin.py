9d726527133cc42d0ecf9fa8bd4d879d7bcb7568
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-02 12:46:42 +0100

    added media root
    added media root and created admin .py

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_director', 'genre', 'rating', 'created_at', 'user')
+    search_fields = ('title', 'author_director', 'genre')
