a9bc9964a254dd58164a3ae3ca323896754715e9
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-03 16:51:13 +0100

    created class RegisterView
    created views to user registration usin g password, username and emaisl

--- reviews/views.py
+++ reviews/views.py
@@ -2,8 +2,38 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Review
 from .serializers import ReviewSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not username or not password:
+            return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
