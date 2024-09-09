7aa226db7ee0384feb337f143488bc4eea06c12d
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-09 13:38:16 +0100

    created user serializer
    created class user serializer with user details fields

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import Review
+from django.contrib.auth.models import User
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
