67f8f568c645fb06ed2fdffc2c57d6997081562c
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-17 10:51:58 +0100

    created profileserializer class
    added class to handle user profile img
    created update to get user data and edit

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -9,18 +9,32 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
+
 class UserSerializer(serializers.ModelSerializer):
-    profile_image = serializers.SerializerMethodField()
+    profile_image = ProfileSerializer()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name',
                   'last_name', 'profile_image']
 
-    def get_profile_image(self, user):
-        profile = Profile.objects.get(user=user)
-        request = self.context.get('request')
-        if profile.image:
-            image_url = profile.image.url
-            return request.build_absolute_uri(image_url)
-        return None
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', None)
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name)
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        if profile_data:
+            profile = instance.profile
+            profile.image = profile_data.get('image', profile.image)
+            profile.save()
+
+        return instance
