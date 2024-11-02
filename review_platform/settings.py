49463f53d27099e59c01c7b3e5f80446fb86dbb5
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-11-02 20:59:47 +0000

    updated secret_key and allowed_hosts

--- review_platform/settings.py
+++ review_platform/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://books-and-films-api-e4ea62133d4f.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
