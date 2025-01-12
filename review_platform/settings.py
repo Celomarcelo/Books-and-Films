1e77e491a4fc0abfb1729a29e1ed4132bee9425a
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2025-01-12 20:51:49 +0000

    updated allowed hosts

--- review_platform/settings.py
+++ review_platform/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['books-and-films-api-12373ad9b35f.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['books-and-films-api-12373ad9b35f.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 
