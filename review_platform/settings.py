4edcff9c4444cda5d05db518b61c3900224c8343
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2025-01-12 15:11:28 +0000

    delete allowed host

--- review_platform/settings.py
+++ review_platform/settings.py
@@ -75,7 +75,6 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://books-and-films-e41e6d4b185b.herokuapp.com",
-    "https://books-and-films-api-10d605724783.herokuapp.com",
 ]
 
 CORS_ALLOW_HEADERS = [
